6e78f602b8d2dc81f7b4e7e8a514c40f30c8dcf8
AppVeyor <ci@appveyor.com>
2023-01-16 21:03:34 +0700

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
-  
+  const fromOneToNineteen = ['','one','two','three','four', 'five','six','seven','eight','nine', 'ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
+  const tens = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+  let word;
+
+  const numStr = number.toString();
+
+  if(number === 0) {
+    return word = 'zero';
+  }
+
+  if(number < 20) {
+    word = fromOneToNineteen[number];
+  } else if(numStr.length === 3 && +numStr[1] === 0) {
+    word = fromOneToNineteen[numStr[0]] + ' hundred ' + fromOneToNineteen[numStr[2]];
+  } else if(numStr.length === 3 && +numStr[1] !== 1) {
+    word = fromOneToNineteen[numStr[0]] + ' hundred ' + tens[numStr[1]] + ' ' + fromOneToNineteen[numStr[2]];
+  } else if(numStr.length === 3 && +numStr[1] === 1) {
+    word = fromOneToNineteen[numStr[0]] + ' hundred ' + fromOneToNineteen[+(numStr[1] + numStr[2])];
+  } else {
+    word = tens[numStr[0]] + ' ' + fromOneToNineteen[numStr[1]]; 
+  }
+
+  return word.trim();
 }
